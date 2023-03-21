f06864f52048a93d334763400e0fc6105dd99422
mstkm <99125333+mstkm@users.noreply.github.com>
2023-03-21 14:44:54 +0700

    add authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        Gate::define('admin', function (User $user) {
+          return $user->username === "mugimustakim";
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\AdminCategoryController;
+use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
 
 /*
@@ -56,11 +57,12 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/dashboard', function() {
   return view('dashboard.index', [
-    'title' => 'Dashboard'
+    'title' => 'Dashboard',
+    'posts' => Post::where('user_id', auth()->user()->id)->get()
   ]);
 })->middleware('auth');
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
